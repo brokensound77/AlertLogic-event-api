88bf387f17c27c8d14c43ef3982265dbb288e496
brokensound77 <justin.s.ibarra@gmail.com>
2017-01-05 23:33:01 -0600

    created init

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup(
+    name='AlertLogic_event_api',
+    version='1.1.1',
+    packages=['al_event_api'],
+    package_dir={'': 'src'},
+    url='https://github.com/brokensound77/AlertLogic-event-api',
+    license='MIT',
+    author='brokensound77',
+    author_email='justin.s.ibarra@gmail.com',
+    description='Pseudo API for Alert Logic events'
+)
