53032bd23686051585e5d101833585bf1921e271
brokensound77 <justin.s.ibarra@gmail.com>
2017-01-11 22:14:18 -0800

    al_api get_incident; argparse for script

--- script.py
+++ script.py
@@ -2,27 +2,28 @@
 """ Script to query the API directly for """
 
 import argparse
-import al_api
+import src.al_api as al_api
 
-
-parser = argparse.ArgumentParser(description='script to implement the al_api')
+description = ('This script is used to implement the al_api in a quick manner. The script will run on the incident'
+               'provided and will return only those events matching ALL the parameters passed. The default is to print'
+               'out these events.')
+parser = argparse.ArgumentParser(description=description)
 parser.add_argument('incident_id', help='AL incident id')
 parser.add_argument('customer_id', default='all_children', help='AL customer id')
-parser.add_argument('-a', '--api_key')
-parser.add_argument('-u', '--username')
-parser.add_argument('-p', '--password')
-parser.add_argument('-iid', '--incident-id')
-parser.add_argument('-eid', '--event-number')
+parser.add_argument('api_key')
+parser.add_argument('username')
+parser.add_argument('password')
+parser.add_argument('-eid', '--event_number')
 parser.add_argument('-ecl', '--classification')
-parser.add_argument('-eda', '--event-destination-address')
-parser.add_argument('-edp', '--event-destination-port')
-parser.add_argument('-esn', '--event-signature')
-parser.add_argument('-esp', '--event-source-port')
-parser.add_argument('-esa', '--event-source-address')
-parser.add_argument('-erh', '--event-request-host')
-parser.add_argument('-ert', '--event-restful-call')
-parser.add_argument('-erc', '--event-response-code')
-parser.add_argument('-esid', '--event-signature-id')
+parser.add_argument('-eda', '--event_destination_address')
+parser.add_argument('-edp', '--event_destination_port')
+parser.add_argument('-esn', '--event_signature')
+parser.add_argument('-esp', '--event_source_port')
+parser.add_argument('-esa', '--event_source_address')
+parser.add_argument('-erh', '--event_request_host')
+parser.add_argument('-ert', '--event_restful_call')
+parser.add_argument('-erc', '--event_response_code')
+parser.add_argument('-esid', '--event_signature_id')
 args = parser.parse_args()
 
-
+incident = al_api.Incident(args.incident_id, args.customer_id, args.api_key, args.username, args.password)
