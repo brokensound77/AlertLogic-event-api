56f33cb0d3dd9718c59ae085fe2ac77499004788
brokensound77 <justin.s.ibarra@gmail.com>
2017-01-15 07:38:57 -0600

    cleanup

--- alapi/alertlogic.py
+++ alapi/alertlogic.py
@@ -2,7 +2,6 @@
     the subclass Incident or with the appropriate methods. Customer errors are also defined for use throughout the
     program. Finally, the requests Session is declared here, outside of the class in order to preserve throughout use.
 """
-#TODO: update
 
 import requests
 from errors import *
